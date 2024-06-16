2271ebd953fd6c06e512379d91660d500b19a176
Das0078 <dasdassd007@gmail.com>
2024-06-16 16:36:45 +0530

    function greetings2 removed

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ console.log("hello world");
 function greetings(name){
     console.log(`Hello ${name}`);
 }
-function greetings2(name){
-    console.log(`Hello2 ${name}`);
-}
+
 greetings("Sachin");
